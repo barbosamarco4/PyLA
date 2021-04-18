2a21d7884d88d0eaa255b096fbdc59bcd739d4b2
DrPud1m <barbosamarco4@gmail.com>
2021-04-18 11:35:43 +0100

    Add typechecker

--- PyLA.py
+++ PyLA.py
@@ -1,34 +1,56 @@
 class Value():
-    pass
+    def __init__(self,name,value=None,valueType=NoneType):
+        TypeChecker(name,value,valueType)
+        self.name      = name
+        self.value     = value
+        self.valueType = valueType
 
-class Set( Value ):
-    def __init__(self,values):
-        self.values = values
-class Function( Value ):
-    def Except(self,pos,value):
-        return (pos,value)
-class Tuple( Value ):
-    def __init__(self,value):
-        self.value = value
-
-class Literal( Value ):
-    def __init__(self,value):
-        self.value = value
-
-class Symbol:
-    def __init__(self,name,value):
-        self.name = name
-        if isinstance(value,list) or isinstance(value,tuple):
-            self.value = Tuple(value)
-        if isinstance(value,dict):
-            self.value = Function(value)
-        if isinstance(value,set):
-            self.value = Set(value)
-        if isinstance(value,int) or isinstance(value,str):
-            self.value = value
     def __str__(self):
         return self.name
-    #TODO: check if other is Conjunction
+
+    def __getitem__(self,key):
+        if isinstance(self.valueType, (Record,Tuple,Sequence)):
+            return Value(f'{self.name}[{key}]',valueType=self.valueType.child)
+        elif isinstance((self.valueType, Function)):
+            return Value(f'{self.name}[{key}]',valueType=self.valueType.image)
+        else:
+            raise TypeError(f'Can\'t index an instance of {self.valueType}')
+
+    def __eq__(self,other):
+        if isinstance(self,(int,Int)) and isinstance(other,(int,Int)):
+            return BinaryPredicate(self,'=',other)
+    def __ne__(self,other):
+        return BinaryPredicate(self,'#',other)
+    def __lt__(self,other):
+        return BinaryPredicate(self,'<',other)
+    def __le__(self,other):
+        return BinaryPredicate(self,'<=',other)
+    def __gt__(self,other):
+        return BinaryPredicate(self,'>', other)
+    def __ge__(self,other):
+        return BinaryPredicate(self,'>=', other)
+
+    #TODO: check if vars are Int
+    def __add__(self,other):
+        return Expr(self,'+',other)
+    def __sub__(self,other):
+        return Expr(self,'-',other)
+    def __mul__(self,other):
+        return Expr(self,'*',other)
+
+    def __invert__(self):
+        return UnaryPredicate('~',self.name)
+
+    #TODO: check if other is Set/Abstract
+    def in(self,other):
+        return BinaryPredicate(self,'\in',other)
+
+class Expr:
+    def __init__(self,lhs,op,rhs):
+        self.lhs = lhs
+        self.op = op
+        self.rhs = rhs
+
     def __eq__(self,other):
         return BinaryPredicate(self,'=',other)
     def __ne__(self,other):
@@ -41,37 +63,6 @@ class Symbol:
         return BinaryPredicate(self,'>', other)
     def __ge__(self,other):
         return BinaryPredicate(self,'>=', other)
-
-    def __add__(self,other):
-        return Expr(self,'+',other)
-    def __sub__(self,other):
-        return Expr(self,'-',other)
-    def __mul__(self,other):
-        return Expr(self,'*',other)
-    #TODO: check if vars are bool
-    def __and__(self,other):
-        return Conjunction(self,other)
-    def __or__(self,other):
-        return Disjunction(self,other)
-    #TODO: index by expression, multiple indexation
-    def __getitem__(self,key):
-        return Expr(self,'[]',key)
-
-class Constant( Symbol ):
-    pass
-
-class Variable ( Symbol ):
-    def __init__(self,name,value):
-        super().__init__(name,value)
-        self.next = f'{name}\'' 
-    def __invert__(self):
-        return UnaryPredicate('~',self.name)
-
-class Expr:
-    def __init__(self,lhs,op,rhs): 
-        self.lhs = lhs
-        self.op  = op
-        self.rhs = rhs
 
 class Predicate:
     def __and__(self,other):
@@ -92,7 +83,7 @@ class BinaryPredicate (Predicate):
         self.op  = op
         self.rhs = rhs
     def __str__(self):
-        return str(self.rhs) + self.op + str(self.lhs)
+        return f'{self.lhs} {self.op} {self.rhs}'
 
 class Formula:
     #formulas because they can be a predicate or a formula
@@ -109,25 +100,27 @@ class Conjunction( Formula ):
             return self
         if isinstance(other,Formula):
             return Conjunction(self,other)
-        if isinstance(other,Variable):
+        if isinstance(other,Bool):
             return Conjunction(self,UnaryPredicate('',other))
     def __or__(self,other):
         if isinstance(other,Predicate) or isinstance(other,Formula):
             return Disjunction(self,other)
-        if isinstance(other,Variable):
+        if isinstance(other,Bool):
             self.formulas.append(UnaryPredicate('',other))
             return self
     def print(self,tabs=''):
         for f in self.formulas:
             if isinstance(f,Formula):
+                print('/\\', end='')
                 f.print(tabs+'\t')
             else:
-                print(f'{tabs}/\{f}')
+                print(f'{tabs}/\ {f}')
+
 class Disjunction( Formula ):
     def __and__(self,other):
         if isinstance(other,Predicate) or isinstance(other,Formula):
             return Conjunction(self,other)
-        if isinstance(other,Variable):
+        if isinstance(other,Bool):
             return Conjunction(self,UnaryPredicate('',other))
     def __or__(self,other):
         if isinstance(other,Predicate):
@@ -135,15 +128,16 @@ class Disjunction( Formula ):
             return self
         if isinstance(other,Formula):
             return Disjunction(self,other)
-        if isinstance(other,Variable):
+        if isinstance(other,Bool):
             self.formulas.append(UnaryPredicate('',other))
             return self
     def print(self,tabs=''):
         for f in self.formulas:
             if isinstance(f,Formula):
+                print('\\/', end='')
                 f.print(tabs+'\t')
             else:
-                print(f'{tabs}\/{f}')
+                print(f'{tabs}\/ {f}')
 
 class Step:
     def __init__(self,name,formula):
@@ -152,10 +146,12 @@ class Step:
 
 class Definition:
     #example: Messages == {x \in 1..10 | x % 2 = 0}
-    def __init__(self,name, expr)
+    def __init__(self,name, expr):
+        pass
 class TLA:
     def __init__(self):
         self.constants = { }
         self.variables = set ()
-        self.steps = []
         self.definitions = []
+    def addStep(self,steps):
+        pass
